b819827667885a78bf09213e7a4c482c1dae6b8c
AdarshSNair13 <adarshsnair13@gmail.com>
2022-01-27 13:48:34 +0530

    port issue fixed

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 
 const app = express();
 
-const PORT = 4000 || process.env.PORT;
+const PORT = process.env.PORT || 4000 ; 
 
 app.get("/", (req,res) => {
     res.status(200).send("<h1>Hello from </h1>");
